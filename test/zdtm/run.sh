99b83afeecba0c1676793def56a771b866131404
Pavel Emelyanov <xemul@parallels.com>
2014-09-12 21:17:02 +0400

    test: Add migration in between
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- test/zdtm/run.sh
+++ test/zdtm/run.sh
@@ -8,13 +8,27 @@ if ! ./ct_init ct.log init.pid ./ct_init.py ${CRIU_TESTS} tests; then
 	exit 1
 fi
 
-# Add migration here
-
 PID=$(cat init.pid)
+echo "Tests started at ${PID}"
+
+echo "Start phaul service"
+../../p.haul-service > ph-srv.log 2>&1 &
+PHSPID=$!
+
+echo "Migrating"
+if ! ../../p.haul pid ${PID} "127.0.0.1" -v=4 --keep-images; then
+	echo "Migration failed"
+	kill -TERM ${PID}
+	kill -TERM ${PHSPID}
+	exit 1
+fi
+
+echo "Checking results"
 kill -TERM ${PID}
 while kill -0 ${PID}; do
 	echo "Waiting to die"
 	sleep ".1"
 done
+kill -TERM ${PHSPID}
 
 tail -n1 ct.log
