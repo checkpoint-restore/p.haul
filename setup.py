18e7b18f2879e4af1155884aa018a78a69a7de73
Nikita Spiridonov <nspiridonov@odin.com>
2015-04-30 16:10:24 +0400

    p.haul: reform project to simplify installation
    To simplify installation and packaging move all python logic to
    phaul subdirectory. Add setup.py and phaul/__init__.py files needed
    for packaging. These changes pack greater part of logic into
    'phaul' python package that can be installed using setup tools and
    used with the help of import directive.
    
    Signed-off-by: Nikita Spiridonov <nspiridonov@odin.com>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+
+setup(name='phaul',
+      description='Tool to live-migrate containers and processes',
+      license='GPLv2',
+      packages=['phaul'],
+      )
