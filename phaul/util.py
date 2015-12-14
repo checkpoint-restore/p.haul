3468660a53a776d88700b74914a19e3d9c47ea54
Nikita Spiridonov <nspiridonov@odin.com>
2015-12-14 14:15:29 +0400

    p.haul: remove unused util.path_to_fs function
    util.path_to_fs no longer needed, so remove it.
    
    Signed-off-by: Nikita Spiridonov <nspiridonov@odin.com>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- phaul/util.py
+++ phaul/util.py
@@ -46,18 +46,6 @@ class net_dev:
 		self.link = link
 
 
-def path_to_fs(path):
-	dev = os.stat(path)
-	dev_str = "%d:%d" % (os.major(dev.st_dev), os.minor(dev.st_dev))
-	mfd = open("/proc/self/mountinfo")
-	for ln in mfd:
-		ln_p = ln.split(None, 9)
-		if dev_str == ln_p[2]:
-			return ln_p[8]
-
-	return None
-
-
 def ifup(ifname):
 	logging.info("\t\tUpping %s", ifname)
 	os.system("ip link set %s up" % ifname)
